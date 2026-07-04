b16eb95c6811520faa32db5296ed1c76a23af666
Anshul Thathera <anshulkasera35@gmail.com>
2026-07-04 22:05:53 +0530

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -118,6 +118,20 @@ export default async function RootLayout({ children }) {
             `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xh9c1kw7x2");
+            `,
+          }}
+        />
         {/* WebSite Schema for Site Name */}
         <script
           type="application/ld+json"
